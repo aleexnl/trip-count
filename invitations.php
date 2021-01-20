7701ed3ef2280357107b7e9a1484018c6010914d
jcsalinas20 <jcsalinasn20@gmail.com>
2021-01-20 02:42:30 +0100

    delete las input element in invitation page

--- invitations.php
+++ invitations.php
@@ -7,7 +7,7 @@
     <title>Invitaciones</title>
     <script src="https://kit.fontawesome.com/b17b075250.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="header.css">
-	<link rel="stylesheet" type="text/css" href="footer.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
     <?php
     session_start();
     $tripName = isset($_SESSION['trip_name']) ? $_SESSION['trip_name'] : header("location:login.php?status=session_expired");
@@ -136,10 +136,11 @@
         }
 
         form.invitations>div.box-btn {
-            width: 50%;
             margin-top: 20px;
             display: flex;
             justify-content: space-evenly;
+            align-items: center;
+            width: 80%;
         }
 
         form.invitations>div.box-btn>button:focus {
@@ -178,37 +179,38 @@
             border: 1px solid #a0342c;
             box-shadow: 0 0 5px #f44336;
         }
-	    
-	main { 
-	    min-height: 82vh;
-	}
+
+        main {
+            min-height: 82vh;
+        }
     </style>
 </head>
 
 <body>
-    <?php require_once('header.php'); ?>
+    <?php include_once('header.php'); ?>
     <main>
-    <p class="title">Invitaciones</p>
-    <p class="destiny">Introduce los correos de tus compañer@s con los que vas a viajar.<br> <i class="fas fa-plane"></i><?= $tripName ?><i class="fas fa-plane"></i></p>
-    <div class="container-messages">
-        <?php
-        if ($has_errors) // If user had errors during log in
-            foreach ($error_messages as $key => $error)
-                echo $error . "</br>";
-        ?>
-    </div>
-    <form class="invitations" action="./invitations.php" method="post">
-        <div class="box-mail">
-            <label for="email-1">Correo 1:</label>
-            <input type="email" class="mails" name="email-1" placeholder="user@mail.com">
+        <p class="title">Invitaciones</p>
+        <p class="destiny">Introduce los correos de tus compañer@s con los que vas a viajar.<br> <i class="fas fa-plane"></i><?= $tripName ?><i class="fas fa-plane"></i></p>
+        <div class="container-messages">
+            <?php
+            if ($has_errors) // If user had errors during log in
+                foreach ($error_messages as $key => $error)
+                    echo $error . "</br>";
+            ?>
         </div>
-        <div class="box-btn">
-            <button class="add-mail button-primary" type="button">Añadir otro correo <i class="fas fa-plus-square fa-v-align"></i></button>
-	    <button class="send button-primary" type="submit" accesskey="e"><u>E</u>nviar Invitaciones <i class="fas fa-paper-plane fa-v-align"></i></button>
-        </div>
-    </form>
+        <form class="invitations" action="./invitations.php" method="post">
+            <div class="box-mail">
+                <label for="email-1">Correo 1:</label>
+                <input type="email" class="mails" name="email-1" placeholder="user@mail.com">
+            </div>
+            <div class="box-btn">
+                <button class="remove-mail button-primary" type="button">Eliminar correo <i class="fas fa-minus-square fa-v-align"></i></button>
+                <button class="add-mail button-primary" type="button">Añadir correo <i class="fas fa-plus-square fa-v-align"></i></button>
+                <button class="send button-primary" type="submit" accesskey="e"><u>E</u>nviar Invitaciones <i class="fas fa-paper-plane fa-v-align"></i></button>
+            </div>
+        </form>
     </main>
-    <?php require_once('footer.php'); ?>
+    <?php include_once('footer.php'); ?>
     <script>
         function generateMessages(type, text, parentName, seconds) {
             let parent = document.getElementsByClassName(parentName)[0];
@@ -275,6 +277,16 @@
             sibling.parentNode.insertBefore(divBoxMail, sibling);
         }
 
+        function removeLastMail() {
+            let mails = document.getElementsByClassName("mails");
+            if (mails.length > 1) {
+                let element = mails[mails.length - 1].parentElement;
+                let parent = element.parentElement;
+                parent.removeChild(element);
+                generateMessages("info", "INFO: Se ha eliminado el último correo.", "container-messages", 4);
+            } else generateMessages("info", "INFO: No se pueden borrar todos los correos.", "container-messages", 4);
+        }
+
         document.getElementsByClassName("invitations")[0].onsubmit = (e) => {
             e.preventDefault();
             validateInvitationsMails() ? e.currentTarget.submit() : null;
@@ -284,6 +296,11 @@
             e.preventDefault();
             generateMessages("info", "INFO: Se ha agregado un nuevo correo.", "container-messages", 4);
             createInputMail();
+        }
+
+        document.getElementsByClassName("remove-mail")[0].onclick = (e) => {
+            e.preventDefault();
+            removeLastMail();
         }
     </script>
 </body>
